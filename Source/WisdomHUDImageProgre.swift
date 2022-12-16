3dc594be028443f501fd393a4d813d74d7c47f10
jianfeng.tang <jianfeng.tang@reacheng.com>
2022-12-16 17:54:31 +0800

    update

--- Source/WisdomHUDImageProgre.swift
+++ Source/WisdomHUDImageProgre.swift
@@ -55,7 +55,7 @@ extension WisdomHUDImageProgreView {
     
     private lazy var circleLayer: CAShapeLayer = {
         let path = UIBezierPath(arcCenter: CGPoint(x: size/2, y: size/2),
-                                   radius: size/2.0-progreWidth,
+                                   radius: (size-progreWidth)/2.0,
                                startAngle: Double.pi,
                                  endAngle: Double.pi*3,
                                 clockwise: true)
@@ -72,7 +72,7 @@ extension WisdomHUDImageProgreView {
     
     private lazy var task_circleLayer: CAShapeLayer = {
         let path = UIBezierPath(arcCenter: CGPoint(x: size/2, y: size/2),
-                                   radius: size/2.0-progreWidth,
+                                   radius: (size-progreWidth)/2.0,
                                startAngle: Double.pi*1.5,
                                  endAngle: Double.pi*3.5,
                                 clockwise: true)
@@ -89,6 +89,7 @@ extension WisdomHUDImageProgreView {
     
     @objc public init(size: CGFloat, barStyle: WisdomSceneBarStyle) {
         super.init(size: size)
+        progreWidth = size/15.0
         switch barStyle {
         case .dark:  circleColor = UIColor.white
         case .light: circleColor = UIColor.black
@@ -134,7 +135,7 @@ extension WisdomHUDImageProgreView {
 
     @objc public private(set) var lineColor = UIColor.white
     
-    private lazy var progreHeight: CGFloat = { return 5.8 }()//size/7.7
+    private lazy var progreHeight: CGFloat = { return size/8.4 }()
     
     private lazy var borderView: UIView = {
         let view = UIView()
@@ -322,13 +323,15 @@ extension WisdomHUDImageProgreView {
         if progre == 1 {
             path.addLine(to: CGPoint(x: water_margin*2+size, y: 0))
         }else if water_leftTopAnim {
+            let random = getArc4random()
             path.addCurve(to: CGPoint(x: water_margin*2+size, y: size*(1-progre)),
-                          controlPoint1: CGPoint(x: left_bottom_x, y: (progre <= 0.12) ? size*(1-progre)-water_deep/2:size*(1-progre)-water_deep),
-                          controlPoint2: CGPoint(x: right_top_x, y: (progre <= 0.12) ? size*(1-progre)+water_deep/2:size*(1-progre)+water_deep))
+                          controlPoint1: CGPoint(x: left_bottom_x, y: ((progre <= 0.1) ? size*(1-progre)-water_deep*0.7:size*(1-progre)-water_deep)-random),
+                          controlPoint2: CGPoint(x: right_top_x, y: ((progre <= 0.1) ? size*(1-progre)+water_deep*0.7:size*(1-progre)+water_deep)+random))
         }else {
+            let random = getArc4random()
             path.addCurve(to: CGPoint(x: water_margin*2+size, y: size*(1-progre)),
-                          controlPoint1: CGPoint(x: left_bottom_x, y: (progre <= 0.12) ? size*(1-progre)+water_deep/2:size*(1-progre)+water_deep),
-                          controlPoint2: CGPoint(x: right_top_x, y: (progre <= 0.12) ? size*(1-progre)-water_deep/2:size*(1-progre)-water_deep))
+                          controlPoint1: CGPoint(x: left_bottom_x, y: ((progre <= 0.1) ? size*(1-progre)+water_deep*0.7:size*(1-progre)+water_deep)+random),
+                          controlPoint2: CGPoint(x: right_top_x, y: ((progre <= 0.1) ? size*(1-progre)-water_deep*0.7:size*(1-progre)-water_deep)-random))
         }
 
         path.addLine(to: CGPoint(x: water_margin*2+size, y: size+water_deep/3))
@@ -344,6 +347,10 @@ extension WisdomHUDImageProgreView {
         waterLayer.add(basicAnim, forKey: nil)
         
         water_leftTopAnim = !water_leftTopAnim
+        
+        func getArc4random()->CGFloat{
+            return 0//CGFloat(arc4random() % UInt32(water_deep/3))-water_deep/3
+        }
     }
 }
 
