20a572607fd656e3d7eedb2ecc06a3ff27fa6add
tangjianfeng <tangjianfeng.vs@gmail.com>
2026-05-21 19:59:23 +0800

    no message

--- Source/HUD+Extension.swift
+++ Source/HUD+Extension.swift
@@ -19,9 +19,10 @@ import AppKit
 #endif
 
 
-//TODO: Extension UIView
-extension UIView {
-    
+// MARK: - WisdomHUDView 布局/约束辅助
+
+extension WisdomHUDView {
+
     // MARK: - set layout size
     @objc public func wisdom_addConstraint(width: CGFloat, height: CGFloat) {
         if width > 0 {
@@ -45,15 +46,15 @@ extension UIView {
     }
     
     // MARK: - set layout center
-    @objc public func wisdom_addConstraint(toCenterX xView: UIView?, toCenterY yView: UIView?) {
+    @objc public func wisdom_addConstraint(toCenterX xView: WisdomHUDView?, toCenterY yView: WisdomHUDView?) {
         wisdom_addConstraint(toCenterX: xView, constantx: 0, toCenterY: yView, constanty: 0)
     }
     
     // MARK: - set layout center offset
-    @objc public func wisdom_addConstraint(toCenterX xView: UIView?,
-                                                 constantx: CGFloat,
-                                           toCenterY yView: UIView?,
-                                                 constanty: CGFloat) {
+    @objc public func wisdom_addConstraint(toCenterX xView: WisdomHUDView?,
+                                           constantx: CGFloat,
+                                           toCenterY yView: WisdomHUDView?,
+                                           constanty: CGFloat) {
         if let xView = xView {
             addConstraint(NSLayoutConstraint(item: xView,
                                              attribute: .centerX,
@@ -73,24 +74,25 @@ extension UIView {
                                              constant: constanty))
         }
     }
-    
-    // MARK: - set layout edage inset
-    @objc public func wisdom_addConstraint(to view: UIView, edageInset: UIEdgeInsets) {
+
+    // MARK: - set layout edge inset
+    @objc public func wisdom_addConstraint(to view: WisdomHUDView, edgeInset: WisdomHUDEdgeInsets) {
         wisdom_addConstraint(with: view,
-                          topView: self,
-                         leftView: self,
-                       bottomView: self,
-                        rightView: self,
-                        edgeInset: edageInset)
+                             topView: self,
+                             leftView: self,
+                             bottomView: self,
+                             rightView: self,
+                             edgeInset: edgeInset)
     }
-    
-    // MARK: - set layout edage inset to UIView
-    @objc public func wisdom_addConstraint(with view: UIView,
-                                             topView: UIView?,
-                                            leftView: UIView?,
-                                          bottomView: UIView?,
-                                           rightView: UIView?,
-                                           edgeInset: UIEdgeInsets) {
+
+    // MARK: - set layout edge inset to view
+    @objc public func wisdom_addConstraint(with view: WisdomHUDView,
+                                           topView: WisdomHUDView?,
+                                           leftView: WisdomHUDView?,
+                                           bottomView: WisdomHUDView?,
+                                           rightView: WisdomHUDView?,
+                                           edgeInset: WisdomHUDEdgeInsets) {
+        let focusing = WisdomHUD_FocusingIdentifier()
         if let topView = topView {
             let topConstraint = NSLayoutConstraint(item: view,
                                                    attribute: .top,
@@ -99,7 +101,7 @@ extension UIView {
                                                    attribute: .top,
                                                    multiplier: 1.0,
                                                    constant: edgeInset.top)
-            topConstraint.identifier = WisdomHUDCore.getWisdomHUD_Focusing()
+            topConstraint.identifier = focusing
             addConstraint(topConstraint)
         }
         if let leftView = leftView {
@@ -110,29 +112,29 @@ extension UIView {
                                                     attribute: .left,
                                                     multiplier: 1.0,
                                                     constant: edgeInset.left)
-            leftConstraint.identifier = WisdomHUDCore.getWisdomHUD_Focusing()
+            leftConstraint.identifier = focusing
             addConstraint(leftConstraint)
         }
         if let bottomView = bottomView {
             let bottomConstraint = NSLayoutConstraint(item: view,
-                                                      attribute: .bottom,
-                                                      relatedBy: .equal,
-                                                      toItem: bottomView,
-                                                      attribute: .bottom,
-                                                      multiplier: 1.0,
-                                                      constant: edgeInset.bottom)
-            bottomConstraint.identifier = WisdomHUDCore.getWisdomHUD_Focusing()
+                                                     attribute: .bottom,
+                                                     relatedBy: .equal,
+                                                     toItem: bottomView,
+                                                     attribute: .bottom,
+                                                     multiplier: 1.0,
+                                                     constant: edgeInset.bottom)
+            bottomConstraint.identifier = focusing
             addConstraint(bottomConstraint)
         }
         if let rightView = rightView {
             let rightConstraint = NSLayoutConstraint(item: view,
-                                                     attribute: .right,
-                                                     relatedBy: .equal,
-                                                     toItem: rightView,
-                                                     attribute: .right,
-                                                     multiplier: 1.0,
-                                                     constant: edgeInset.right)
-            rightConstraint.identifier = WisdomHUDCore.getWisdomHUD_Focusing()
+                                                    attribute: .right,
+                                                    relatedBy: .equal,
+                                                    toItem: rightView,
+                                                    attribute: .right,
+                                                    multiplier: 1.0,
+                                                    constant: edgeInset.right)
+            rightConstraint.identifier = focusing
             addConstraint(rightConstraint)
         }
     }
@@ -158,55 +160,80 @@ extension WisdomHUDView {
 // MARK: - String 文字尺寸测量
 
 extension String {
-    
-    // MARK: - Get Text Size With: UIFont - CGSize
-    public func wisdom_textSize(font: UIFont, constrainedToSize size: CGSize) -> CGSize {
-        var textSize:CGSize!
-        if size.equalTo(CGSize.zero) {
-            let attributes = NSDictionary(object: font, forKey: NSAttributedString.Key.font as NSCopying)
-            textSize = self.size(withAttributes: attributes as? [NSAttributedString.Key : Any] )
-            
-        } else {
-            let option = NSStringDrawingOptions.usesLineFragmentOrigin
-            let attributes = NSDictionary(object: font, forKey: NSAttributedString.Key.font as NSCopying)
-            
-            let stringRect = self.boundingRect(with: size,
-                                               options: option,
-                                               attributes: attributes as? [NSAttributedString.Key : Any],
-                                               context: nil)
-            textSize = stringRect.size
+
+    // MARK: - Get Text Size With WisdomHUDFont
+    public func wisdom_textSize(font: WisdomHUDFont, constrainedToSize size: CGSize) -> CGSize {
+        let attributes: [NSAttributedString.Key: Any] = [.font: font]
+        if size.equalTo(.zero) {
+            return self.size(withAttributes: attributes)
         }
-        return textSize
+        let stringRect = self.boundingRect(with: size,
+                                           options: .usesLineFragmentOrigin,
+                                           attributes: attributes,
+                                           context: nil)
+        return stringRect.size
     }
 }
 
 
-extension UIColor {
-    
-    // MARK: - Get Color with: String
-    @objc public static func wisdom_color(hex: String, alpha: CGFloat=1) -> UIColor {
-        var color_hex = hex+"000000"
+// MARK: - WisdomHUDColor hex 解析
+
+extension WisdomHUDColor {
+
+    // MARK: - Get Color with hex String
+    @objc public static func wisdom_color(hex: String, alpha: CGFloat = 1) -> WisdomHUDColor {
+        var color_hex = hex + "000000"
         if hex.hasPrefix("0x") || hex.hasPrefix("0X") {
             color_hex.removeFirst()
             color_hex.removeFirst()
-        }else if hex.hasPrefix("#") {
+        } else if hex.hasPrefix("#") {
             color_hex.removeFirst()
         }
-
         let start = color_hex.startIndex
         let end = color_hex.index(start, offsetBy: 6)
         color_hex = String(color_hex[start ..< end])
-        
+
         let scanner = Scanner(string: color_hex)
-        scanner.scanLocation = 0
-            
         var rgbValue: UInt64 = 0
         scanner.scanHexInt64(&rgbValue)
-            
+
         let r = (rgbValue & 0xff0000) >> 16
         let g = (rgbValue & 0xff00) >> 8
         let b = rgbValue & 0xff
-        return UIColor(red:CGFloat(r)/0xff, green:CGFloat(g)/0xff, blue:CGFloat(b)/0xff, alpha:alpha)
+        return WisdomHUDColor(red: CGFloat(r) / 0xff,
+                              green: CGFloat(g) / 0xff,
+                              blue: CGFloat(b) / 0xff,
+                              alpha: alpha)
+    }
+}
+
+
+// MARK: - macOS 独有:NSBezierPath → CGPath(系统 14 以下没有内建 cgPath)
+
+#if os(macOS)
+extension NSBezierPath {
+
+    @objc public var wisdom_cgPath: CGPath {
+        let path = CGMutablePath()
+        var points = [NSPoint](repeating: .zero, count: 3)
+        for i in 0 ..< self.elementCount {
+            let type = self.element(at: i, associatedPoints: &points)
+            switch type {
+            case .moveTo:
+                path.move(to: points[0])
+            case .lineTo:
+                path.addLine(to: points[0])
+            case .curveTo, .cubicCurveTo:
+                path.addCurve(to: points[2], control1: points[0], control2: points[1])
+            case .quadraticCurveTo:
+                path.addQuadCurve(to: points[1], control: points[0])
+            case .closePath:
+                path.closeSubpath()
+            @unknown default:
+                break
+            }
+        }
+        return path
     }
 }
 #endif
